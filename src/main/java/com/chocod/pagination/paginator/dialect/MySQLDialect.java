2147086268995c5f5fb64b302b61126d3161d672
chocod <chocod@qq.com>
2017-09-14 11:34:20 +0800

    创建项目

---
+++ src/main/java/com/chocod/pagination/paginator/dialect/MySQLDialect.java
@@ -0,0 +1,30 @@
+package com.chocod.pagination.paginator.dialect;
+
+import org.apache.ibatis.mapping.MappedStatement;
+
+import com.chocod.pagination.paginator.domain.PageBounds;
+
+/**
+ * MySql分页方言(mybatis-config.xml中配置)
+ * 
+ */
+public class MySQLDialect extends Dialect {
+
+	public MySQLDialect(MappedStatement mappedStatement, Object parameterObject, PageBounds pageBounds) {
+		super(mappedStatement, parameterObject, pageBounds);
+	}
+
+	protected String getLimitString(String sql, String offsetName, int offset, String limitName, int limit) {
+		StringBuffer buffer = new StringBuffer(sql.length() + 20).append(sql);
+		if (offset > 0) {
+			buffer.append(" limit ?, ?");
+			setPageParameter(offsetName, offset, Integer.class);
+			setPageParameter(limitName, limit, Integer.class);
+		} else {
+			buffer.append(" limit ?");
+			setPageParameter(limitName, limit, Integer.class);
+		}
+		return buffer.toString();
+	}
+
+}
